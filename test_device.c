04be47eb348002774b99667ef6aeb2313077c9da
gushuizerotoone <chengwenzhuce@126.com>
2013-07-03 00:49:24 -0700

    add test case into test_device.c

--- test_device.c
+++ test_device.c
@@ -1,6 +1,32 @@
 
 #include "device.h"
 
+void test_r(){
+   const char * file_name = "r_test_file";
+   fat_dev_t fdv_f;
+   fat_dev_init_r(&fdv_f, file_name);
+
+   char buf[128] = {0,};
+   fat_dev_read(&fdv_f, 0, buf, sizeof(buf) - 1);
+   FAT_PRINT("%s\n", buf);
+   FAT_PRINT("%c %c %c\n", buf[79], buf[80], buf[81]);
+   fat_dev_destroy(&fdv_f);
+}
+
+void test_w(){
+   const char * file_name = "w_test_file";
+
+   char buf[6] = {0x64, 0x12, 0x24, };
+   fat_dev_t fdv_f;
+   fat_dev_create_file(&fdv_f, file_name);
+   fat_dev_write(&fdv_f, 0, buf, 3);
+
+   FAT_PRINT("%02x %02x %02x\n", buf[3], buf[4], buf[5]);
+   fat_dev_read(&fdv_f, 0, buf + 3, 3);
+   FAT_PRINT("%02x %02x %02x\n", buf[3], buf[4], buf[5]);
+   fat_dev_destroy(&fdv_f);
+}
+
 void test_rw()
 {
     // set_up();
@@ -30,5 +56,12 @@ void test_rw()
 void test_device()
 {
     //1. lll......
+    FAT_PRINT("test read...\n");
+    test_r();
+
+    FAT_PRINT("test write...\n");
+    test_w();
+
+    FAT_PRINT("test read&write...\n");
     test_rw();
 }
