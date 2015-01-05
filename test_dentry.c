07097a24e7c230fdc87078e4aa6d1cd51829ca6e
yunccll <yunccll@sina.com>
2015-01-05 00:12:12 -0800

    delete useless testcase

--- test_dentry.c
+++ test_dentry.c
@@ -3,17 +3,6 @@
 #include "dentry.h"
 #include "device.h"
 
-static void test_()
-{
-    fat_dentry_t dentry;
-    memset(&dentry, 0, sizeof(fat_dentry_t));
-
-    dentry.time.val = 0xb966;
-    dentry.date.val = 0x42da;
-
-    FAT_DEBUG("time: %d:%d:%d \n", dentry.time.detail.hour, dentry.time.detail.minute, dentry.time.detail.second);
-    FAT_DEBUG("date: %d-%d-%d \n", dentry.date.detail.year+1980, dentry.date.detail.month, dentry.date.detail.day);
-}
 
 static void test_root_entries_print()
 {
@@ -37,6 +26,5 @@ static void test_root_entries_print()
 
 void test_dentry()
 {
-    test_();
     test_root_entries_print();
 }
