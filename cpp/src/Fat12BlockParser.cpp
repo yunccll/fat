69b58f1b7a21acb9754051af3c4356a318341232
chenglun <yuncll@sina.com>
2018-05-07 18:28:09 +0800

    add more fat info

--- cpp/src/Fat12BlockParser.cpp
+++ cpp/src/Fat12BlockParser.cpp
@@ -8,6 +8,26 @@
 
 #pragma pack(1)
 
+struct Fat {
+    unsigned char numberOfDriver;   // 00 -> floppy disk, 0x80 -> hard disks
+    unsigned char reserved1;        // 0
+    unsigned char bootSignature;    // 0x29 -> Extended boot signature 
+    uint32_t volumeId;              //
+    char volumeLabel[11];           //
+    char fileSystemType[8];            // "FAT12   " "FAT16   " "FAT32   "
+};
+
+struct Fat32 {
+    uint32_t sectorPerFAT;      //only for Fat32
+    uint16_t extendFlag;        //bits 0-3: zero-based number of active Fat, 4-6: reserved, 7: 0/1 8-15: reserved
+    uint16_t fileSystemVersion; // 0:0
+    uint32_t clusterNumberOfRootDirectory;  // 2
+    uint16_t fsInfo;
+    uint16_t backupBootSector;
+    char reserved[12];
+    unsigned char driverNumber;
+    unsigned char reserved1;
+};
 struct BiosParameterBlock{
     uint16_t bytesPerSector;        //512
     unsigned char sectorPerCluster; // TODO: ??
@@ -21,6 +41,10 @@ struct BiosParameterBlock{
     uint16_t numberOfHeaders;       //
     uint32_t hiddenSectors;         // hidden sectors preceding this partition; 0 -> no partition
     uint32_t totalSector32;         //
+    union fatSpecial {
+        struct Fat fatBootSector;
+        struct Fat32 fat32BootSector;
+    } special;
 };
 struct BootSector{
     char jumpToBoot[3];
@@ -34,15 +58,15 @@ int Fat12BlockParser::parseFsMeta(Blocks * blocks, size_t offset, size_t len){
     assert(len == 1);
     BootSector * bootSector = (BootSector*)(blocks->getBlocks(0)->get());
     std::cout << bootSector->oemName << std::endl;
-    //reserverRegion parse
-    //1. auto blks = blocks.get(offset, len); 
-    //2. details parse
-    //3. create TextFsMeta
+    std::cout << bootSector->bpb.special.fatBootSector.fileSystemType << std::endl;
+    //FsInfoBuilder builder;
+    //_fsInfo = builder.bytesPerSector(512).sectorPerCluster(1);
     return 0;
 }
 //TODO:
 int Fat12BlockParser::parseFileAllocator(Blocks * blocks, size_t offset, size_t len){
     assert(len == 18);
+    //offset -->  & len;
     return 0;
 }
 //TODO:
