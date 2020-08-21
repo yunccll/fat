e569f3d2158357f7422d0a1ffe115616a5ed9257
yunccll <yunccll@sina.com>
2020-08-21 15:54:57 +0800

    add RootEntries & Entry . TODO: unittest for entry & rootEntries

--- cpp/src/RootEntries.cpp
+++ cpp/src/RootEntries.cpp
@@ -1,27 +1,88 @@
 #include "RootEntries.h"
 
+#include <cassert>
+#include <sstream>
 
 namespace fat {
 
-
-struct Entry {
-public:
+struct Date{
+    uint16_t day : 5;
+    uint16_t month : 4;
+    uint16_t year : 7;
 };
-
-class RootEntries {
-public:
-    RootEntries(size_t capacity);
-    ~RootEntries();
-
-    bool add(const Entry * entry);
-    bool remove(const char * path);
-    Entry *  getEntry(const char * path) const;
-    bool modify(const char * path, const Entry * newEntry);
-
-    typedef Entry * EntryPointer;
-private:
-    std::unordered_map<std::string, EntryPointer> maps;
+struct Time {
+    uint16_t second : 5;
+    uint16_t minute : 6;
+    uint16_t hour   : 5;
 };
+static std::ostream & operator << (std::ostream & os, const Date & date){
+    os << (uint64_t)(date.year + 1980) <<  "-"  << date.month << "-" << date.day;
+    return os;
+}
+static std::ostream & operator << (std::ostream & os, const Time & time){
+    os << time.hour << ":" << time.minute << ":" << time.second;
+    return os;
+}
+
+
+std::string Entry::getCreateTimeStamp() const{
+    std::ostringstream oss;
+    oss << *reinterpret_cast<const Date*>(&(item.createDate));
+    oss << " " ;
+    oss << *reinterpret_cast<const Time*>(&(item.createTime));
+    return oss.str();
+}
+std::string Entry::getLastAccessDate() const{ //read or write date
+    std::ostringstream oss;
+    oss << *reinterpret_cast<const Date*>(&(item.lastAccessDate));
+    return oss.str();
+}
+std::string Entry::getWriteTimeStamp() const{
+    std::ostringstream oss;
+    oss << *reinterpret_cast<const Date*>(&(item.writeDate));
+    oss << " ";
+    oss << *reinterpret_cast<const Time*>(&(item.writeTime));
+    return oss.str();
+}
+
+std::ostream & operator<< (std::ostream & os, const Entry & entry){
+    //TODO: 
+    return os;
+}
+
+bool RootEntries::add(const char * path, const RootEntries::EntryPointer entry){
+    assert(path != nullptr);
+    auto iter = maps.find(path);
+    if(iter == maps.end()){
+        maps.insert(std::make_pair(path, entry));
+        return true;
+    }
+    return false;
+}
+RootEntries::EntryPointer RootEntries::remove(const char * path){
+    auto iter = maps.find(path);
+    if(iter != maps.end()){
+        maps.erase(iter);
+        return iter->second;
+    }
+    return nullptr;
+}
+RootEntries::EntryPointer RootEntries::getEntry(const char * path) const{
+    auto iter = maps.find(path);
+    if(iter != maps.end()){
+        return iter->second;
+    }
+    return nullptr;
+}
+RootEntries::EntryPointer RootEntries::modify(const char * path, const RootEntries::EntryPointer newEntry){
+    auto iter = maps.find(path);
+    if(iter != maps.end()){
+        auto old = iter->second;
+        iter->second = newEntry;
+        return old;
+    }
+    return nullptr;
+}
 
 
 } //end of namespace fat
--- cpp/src/RootEntries.h
+++ cpp/src/RootEntries.h
@@ -1,5 +1,140 @@
 #pragma once
 
+
+#include <unordered_map>
+#include <string>
+
+#include "Slice.h"
+
 namespace fat {
+
+struct Entry {
+public:
+    Entry(const char * buf, size_t size)
+    {
+        assert(size >= sizeof(struct DirItem));
+        memcpy(&item, buf, std::min(size, sizeof(struct DirItem)));
+    }
+    Entry(const Slice & directory)
+    : Entry(directory.data(), directory.size())
+    {
+    }
+    ~Entry(){
+    }
+
+    Slice getName() const {
+        return Slice(item.name, 11);
+    }
+
+    
+    uint64_t getEntrySize() const {
+        return sizeof(struct DirItem);
+    }
+
+    static const uint8_t xFreeAfterUsed = 0xe5;
+    bool isFreeEntryAfterUsed() const {
+        return (item.name[0] == xFreeAfterUsed);
+    }
+    static const uint8_t xFreeEntry = 0x00;
+    bool isFreeEntry() const {
+        return (item.name[0] == xFreeEntry);
+    }
+
+    static const uint8_t xFakeKanJi = 0x05;
+    bool isFakeKanJi() const {
+        return (item.name[0] == xFakeKanJi);
+    }
+
+    static const uint8_t xSpace = 0x20;
+    bool isSpace() const {
+        return item.name[0] == xSpace;
+    }
+    //  any of name[i] invalid 
+    //   1. values less 0x20 except 0x05, 0x00
+    //   2. 0x22, 0x2A, 0x2B, 0x2C, 0x2E, 0x2F, 
+    //      0x3A, 0x3B, 0x3C, 0x3D, 0x3E, 0x3F, 
+    //      0x5B, 0x5C, 0x5D, and 0x7C
+    
+    //* *********attribute*******/ 
+    enum FileAttrbite {
+        FaReadOnly    = 0x01,
+        FaHidden      = 0x02,
+        FaSystem      = 0x04,
+        FaVolumnId    = 0x08,
+        FaDirectory   = 0x10,
+        FaArchive     = 0x20,
+    };
+    static const uint8_t xLongName = FaReadOnly|FaHidden|FaSystem|FaVolumnId;
+    bool isLongName() const {
+        return (item.attribute & xLongName) == xLongName;
+    }
+    bool isDirectory() const {
+        return (item.attribute & FaDirectory) == FaDirectory;
+    }
+    bool isReadOnly() const {
+        return (item.attribute & FaReadOnly) == FaReadOnly;
+    }
+    bool isHidden() const {
+        return (item.attribute & FaHidden) == FaHidden;
+    }
+    bool isSystem() const {
+        return (item.attribute & FaSystem) == FaSystem;
+    }
+    uint8_t getAttribute() const {
+        return item.attribute;
+    }
+
+
+    std::string getCreateTimeStamp() const;
+    std::string getLastAccessDate() const;
+    std::string getWriteTimeStamp() const;
+
+    uint64_t getFirstCluster() const{
+        return (((uint64_t)item.firstClusterHigh)<<16)||((uint64_t)item.firstClusterLow);
+    }
+    uint64_t getFileSize() const{
+        return item.fileSize;
+    }
+
+private:
+#pragma pack(1)
+    struct DirItem{
+        char  name[11];
+        uint8_t attribute;
+        uint8_t reserved;
+        uint8_t createTimeMilisecondTenth;//milisecond
+        uint16_t createTime;
+        uint16_t createDate;
+        uint16_t lastAccessDate;
+        uint16_t firstClusterHigh;
+        uint16_t writeTime;
+        uint16_t writeDate;
+        uint16_t firstClusterLow;
+        uint32_t fileSize;
+    };
+#pragma pack()
+    DirItem item;
+};
+
+
+std::ostream & operator<< (std::ostream & os, const Entry & entry);
+
+class RootEntries {
+public:
+
+    RootEntries(size_t capacity);
+    ~RootEntries();
+
+    typedef Entry * EntryPointer;
+
+    bool add(const char* path, const EntryPointer entry);
+    EntryPointer remove(const char * path);
+    Entry *  getEntry(const char * path) const;
+    EntryPointer modify(const char * path, const EntryPointer newEntry);
+
+private:
+    std::unordered_map<std::string, EntryPointer> maps;
+};
+
 
 } //end of namespace fat
